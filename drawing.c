03d2c5a21218ae19a8af1441dfbaf3bf1dc86265
LutzGebelman <yuraosipov2012@gmail.com>
2024-08-26 21:47:41 +0300

    Revert "Rendering related files"
    This reverts commit 2bb4035ba9e6c93992ef6ee40a7052b7cea9a48f.

--- drawing.c
+++
@@ -1,7 +0,0 @@
-#include "drawing.h"
-
-// Adjusts scale of player geometry as requested
-PlayerShape ResizePlayerShape(PlayerShape pShape);
-
-// Draws the player based on it's *player.shape.points* array
-int DrawObject(PlayerStruct *player);
