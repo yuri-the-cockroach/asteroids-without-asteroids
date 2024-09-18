c2a79f7852134293b4bfe9b2d506e09d8704bcc5
LutzGebelman <yuraosipov2012@gmail.com>
2024-09-18 23:22:43 +0300

    Rewrite to support object insead of player and more

--- objectlogic.c
+++ objectlogic.c
@@ -1,58 +1,39 @@
+#include "headers/objectlogic.h"
 #include <raylib.h>
-#include <raymath.h>
-#include "headers/playerlogic.h"
+#include <string.h>
 
-void UpdatePlayerPos(PlayerStruct *player) {
-    player->position.x = player->position.x + player->speed.x * GetFrameTime();
-    player->position.y = player->position.y + player->speed.y * GetFrameTime();
+void UpdateObjectPos(ObjectStruct *object) {
+    object->position.x = fmod(object->position.x + object->speed.x * GetFrameTime(), (float)GetScreenWidth());
+    if (object->position.x < 0) object->position.x = (float)GetScreenWidth();
+    object->position.y = fmod(object->position.y + object->speed.y * GetFrameTime(), (float)GetScreenHeight());
+    if (object->position.y < 0) object->position.y = (float)GetScreenHeight();
 }
 
-void OnAccelerate(PlayerStruct *player, float speed) {
-    player->speed.x = player->speed.x + player->shape.points[0].x * speed;
-    player->speed.y = player->speed.y + player->shape.points[0].y * speed;
-}
-
-void UpdatePlayerHeading(PlayerStruct *player) {
-    player->heading = atan2(player->shape.points[0].x, player->shape.points[0].y);
+void OnObjectAccelerate(ObjectStruct *object, float speed) {
+    object->speed.x = object->speed.x + object->shape.points[0].x * speed;
+    object->speed.y = object->speed.y + object->shape.points[0].y * speed;
 }
 
 
-PlayerStruct TransformPlayerMatrix(PlayerStruct * player, float rotationAngle) {
-    PlayerStruct tempPl = *player;
-
-    unsigned int arrayLength = sizeof(player->shape.points) / sizeof(Vector2);
-    for (unsigned int current = 0; current < arrayLength; current++) {
-        tempPl.shape.points[current].x = ( player->shape.points[current].x * cos(rotationAngle) ) - ( player->shape.points[current].y * sin(rotationAngle) );
-        tempPl.shape.points[current].y = ( player->shape.points[current].x * sin(rotationAngle) ) + ( player->shape.points[current].y * cos(rotationAngle) );
+void TransformObjectMatrix(ShapeStruct * shape, float rotationAngle) {
+    for (unsigned int current = 0; current < shape->arrayLength; current++) {
+        shape->points[current].x = ( shape->refPoints[current].x * cos(rotationAngle) ) - ( shape->refPoints[current].y * sin(rotationAngle) );
+        shape->points[current].y = ( shape->refPoints[current].x * sin(rotationAngle) ) + ( shape->refPoints[current].y * cos(rotationAngle) );
     }
-
-    return tempPl;
 }
 
-void MutTransformPlayerMatrix(PlayerStruct * player, float rotationAngle) {
-    PlayerStruct tempPl = *player;
-
-    unsigned int arrayLength = sizeof(player->shape.points) / sizeof(Vector2);
-    for (unsigned int current = 0; current < arrayLength; current++) {
-        tempPl.shape.points[current].x = ( player->shape.points[current].x * cos(rotationAngle) ) - ( player->shape.points[current].y * sin(rotationAngle) );
-        tempPl.shape.points[current].y = ( player->shape.points[current].x * sin(rotationAngle) ) + ( player->shape.points[current].y * cos(rotationAngle) );
-    }
-    *player = tempPl;
-}
-
-void DrawPlayer(PlayerStruct * player) {
+void DrawObject(ObjectStruct * object) {
     int prevPoint = 0;
-    unsigned int arrayLength = sizeof(player->shape.points) / sizeof(Vector2);
 
-    for (unsigned int curPoint = 1; curPoint < arrayLength; curPoint++){
+    for (unsigned int curPoint = 1; curPoint < object->shape.arrayLength; curPoint++){
         DrawLineEx (
             (Vector2) { // Draw from x/y
-                player->shape.points[prevPoint].x + player->position.x,
-                player->shape.points[prevPoint].y + player->position.y
+                object->shape.points[prevPoint].x + object->position.x,
+                object->shape.points[prevPoint].y + object->position.y
             },
             (Vector2) { // Draw to x/y
-                player->shape.points[curPoint].x + player->position.x,
-                player->shape.points[curPoint].y + player->position.y
+                object->shape.points[curPoint].x + object->position.x,
+                object->shape.points[curPoint].y + object->position.y
             },
             2.0, // Thikness of the line
             WHITE // Color of the line
@@ -65,37 +46,48 @@ void DrawPlayer(PlayerStruct * player) {
     DrawLineEx
     (
         (Vector2){ // Draw from x/y
-            player->shape.points[0].x + player->position.x,
-            player->shape.points[0].y + player->position.y
+            object->shape.points[0].x + object->position.x,
+            object->shape.points[0].y + object->position.y
         },
         (Vector2){ // Draw to x/y
-            player->shape.points[arrayLength - 1].x + player->position.x,
-            player->shape.points[arrayLength - 1].y + player->position.y
+            object->shape.points[object->shape.arrayLength - 1].x + object->position.x,
+            object->shape.points[object->shape.arrayLength - 1].y + object->position.y
         },
         1.0,  // Thikness of the line
         WHITE // Color of the line
     );
 }
 
-PlayerShape ResizePlayerShape(PlayerShape pShape) {
-    unsigned int arrayLength = sizeof(pShape.points) / sizeof(Vector2);
-
-    PlayerShape tempPShape;
+Vector2* _ResizeVector2(const Vector2 *vector, float size, unsigned int arrayLength) {
+    Vector2 *tempPShape = malloc(sizeof(Vector2) * arrayLength);
 
     for (unsigned int current = 0; current < arrayLength; current++) {
-        tempPShape.points[current].x = round(pShape.points[current].x * pShape.size);
-        tempPShape.points[current].y = round(pShape.points[current].y * pShape.size);
+        tempPShape[current].x = round(vector[current].x * size);
+        tempPShape[current].y = round(vector[current].y * size);
     }
+
     return tempPShape;
 }
 
-PlayerStruct InitPlayer() {
-    return (PlayerStruct){
-        BASE_ACCELL,            // acceleration speed
-        BASE_ROTATE,            // Rotation speed
+ShapeStruct InitShape(const Vector2 *pointArray, unsigned int arrayLength, float sizeMult) {
+    ShapeStruct toReturn = {
+        sizeMult,
+        arrayLength,
+        calloc(arrayLength, sizeof(Vector2)),
+        (const Vector2*)_ResizeVector2(pointArray, sizeMult, arrayLength),
+        *TransformObjectMatrix
+    };
+    memcpy((void*)toReturn.points, (void*)toReturn.refPoints, sizeof(Vector2) * arrayLength);
+    return toReturn;
+}
+
+ObjectStruct InitObject(ShapeStruct shape) {
+    return (ObjectStruct){
+        0,            // acceleration speed
+        0,            // Rotation speed
         0,                      // Starter heading
-        (Vector2){ 0, 0 },      // Starting position of the player
-        (Vector2){ 0, 0 },      // Starting speed of the player
-        ResizePlayerShape(PLAYER_BASE_SHAPE) // Shape of the player
+        (Vector2){ 0, 0 },      // Starting position of the object
+        (Vector2){ 0, 0 },      // Starting speed of the object
+        shape
     };
 }
