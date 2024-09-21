69860045e4cc5613c6d112ac2632e8d7508f99db
LutzGebelman <yuraosipov2012@gmail.com>
2024-09-22 00:18:29 +0300

    Moved all the constants into struct.h

--- main.c
+++ main.c
@@ -8,80 +8,64 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-#include "src/gamelogic.h"
+// Local Headers
 #include "src/objecthandler.h"
-#include "src/objectlogic.h"
 #include "src/syslogic.h"
-
-const unsigned int BASE_ACCELL = 5;
-const unsigned int BASE_ROTATE = 5;
+#include "src/structs.h"
+#include "src/gamelogic.h"
+#include "src/render.h"
 
 // Retuns prefilled ObjectStruct with default data
 
-int main() {
+
+int main(void) {
     signal(SIGINT, SigIntHandler);
 
-    const int screenWidth = 1280;
-    const int screenHight = 1080;
+    int screenWidth = 1600;
+    int screenHeight = 900;
 
-    int fpsTarget = 120;
+    int fpsTarget = 75;
 
-    InitWindow(screenWidth, screenHight, "asteroids without asteroids");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "asteroids without asteroids");
 
-    SetWindowPosition(2560 + 900 - screenWidth, 0);
+    screenHeight /= 2;
+    SetWindowPosition(0, 0);
     SetWindowMinSize(10, 10);
     SetWindowMaxSize(8192, 8192);
 
     SetTargetFPS(fpsTarget);
 
-    ShapeStruct PLAYER_SHAPE =
-        InitShape(PLAYER_SHAPE_POINTS,
-                  sizeof(PLAYER_SHAPE_POINTS) / sizeof(Vector2),
-                  0.5);
-    ShapeStruct ASTEROID_SHAPE =
-        InitShape(ASTEROID_SHAPE_POINTS,
-                  sizeof(ASTEROID_SHAPE_POINTS) / sizeof(Vector2),
-                  1);
-
     /*  ---------------------<Object Init>--------------------  */
 
     ObjectTracker tracker = InitTracker();
-    CreatePlayer(&tracker, (Vector2){ 200, 200 }, 5, 5);
-    CreateAsteroid(&tracker, (Vector2){ 300, 300 }, (Vector2){ 6, 9 }, 5);
-    CreateAsteroid(&tracker, (Vector2){ 400, 300 }, (Vector2){ 7, 9 }, -2);
+    CreatePlayer(&tracker, (Vector2){ 200, 200 }, 0.5);
+    char debug[128] = "";
 
-    printf("sizeof(PLAYER_SHAPE): %ld\n", sizeof(PLAYER_SHAPE.arrayLength));
-    printf("sizeof(PLAYER_SHAPE): %ld\n", sizeof(Vector2));
     while (!WindowShouldClose()) {
         // Adjust moving speed for current frametime, so you don't get faster as
         // fps goes up
-        /* KeyboardHandler(&player); // All the keybindings and movement */
 
-        /* sprintf(currentRotation, */
-        /*         "pos x: %f y: %f", */
-        /*         player.position.x, */
-        /*         player.position.y); */
-        /* UpdateObjectPos(&asteroid); */
-        /* UpdateObjectPos(&player); */
+        SCREEN_WIDTH = GetScreenWidth();
+        SCREEN_HEIGHT = GetScreenHeight();
 
+        DebugingKeyHandler(&tracker);
         RunActionList(&tracker);
 
-        if (IsKeyPressed('9'))
-            SetTargetFPS(fpsTarget = 75);
-        if (IsKeyPressed('0'))
-            SetTargetFPS(fpsTarget = 0);
-        if (IsKeyPressed('='))
-            SetTargetFPS(fpsTarget += 10);
-        if (IsKeyPressed('-'))
-            SetTargetFPS(fpsTarget -= 10);
         BeginDrawing();
+        {
 
-        ClearBackground(BLACK);
+            ClearBackground(BLACK);
+            DrawAllFromDrawList(&tracker);
 
-        DrawFPS(400, 0);
-        DrawAllFromDrawList(&tracker);
+            sprintf(debug, "Length of the list: %lu", tracker.objListLen);
+            DrawText(debug, 20, 20, 18, RAYWHITE);
 
+            DrawText(debug, 20, 40, 18, RAYWHITE);
+            DrawFPS(400, 0);
+        }
         EndDrawing();
     }
 
--- main.h
+++ main.h
@@ -1,18 +1,5 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#include <raylib.h>
-#include <rcamera.h>
-#include <raymath.h>
-#include <signal.h>
-#include <err.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-// Local Headers
-#include "src/syslogic.h"
-#include "src/objectlogic.h"
-#include "src/gamelogic.h"
 
 #endif // MAIN_H_
