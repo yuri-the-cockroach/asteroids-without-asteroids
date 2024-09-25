7344c11b4a4f0ef74f762f4c44145c68a9c515c7
LutzGebelman <yuraosipov2012@gmail.com>
2024-09-25 03:08:57 +0300

    Visual debugger to easier highlight stuff

---
+++ src/visdebugger.c
@@ -0,0 +1,70 @@
+#include <raylib.h>
+#include <stdio.h>
+
+#include "structs.h"
+#include "visdebugger.h"
+
+#pragma GCC diagnostic ignored "-Wvarargs"
+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
+
+void DebugDisplayText(Vector2 pos, const char *restrict format, ...) {
+    if (!VISUAL_DEBUG)
+        return;
+
+    char messageString[1024] = "";
+
+    va_list argptr;
+
+    int argcount = 0;
+    int i = 0;
+    char current;
+
+    // Count how many arguments we have
+    // by counting format specifiers in a format string
+    do {
+        current = format[i];
+        if (current == '%')
+            argcount++;
+        i++;
+    } while (current != '\0');
+
+    // Pass all the arguments to the vsprinf, which will fill it into the
+    // message string. If argptr is 0, then it will just be ignored (hopefully)
+    va_start(argptr, argcount);
+    vsprintf(messageString, format, argptr);
+    va_end(argptr);
+
+    // Actually print to stderr
+    DrawText(messageString, (int)pos.x, (int)pos.y, 18, WHITE);
+}
+
+/* void DebugDisplayText(Vector2 pos, const char *restrict format, ...) { */
+/*     if (!VISUAL_DEBUG) */
+/*         return; */
+
+/*     char messageString[1024] = ""; */
+
+/*     va_list argptr; */
+
+/*     int argcount = 0; */
+/*     int i = 0; */
+/*     char current; */
+
+/*     // Count how many arguments we have */
+/*     // by counting format specifiers in a format string */
+/*     do { */
+/*         current = format[i]; */
+/*         if (current == '%') */
+/*             argcount++; */
+/*         i++; */
+/*     } while (current != '\0'); */
+
+/*     // Pass all the arguments to the vsprinf, which will fill it into the */
+/*     // message string. If argptr is 0, then it will just be ignored (hopefully) */
+/*     va_start(argptr, argcount); */
+/*     vsprintf(messageString, format, argptr); */
+/*     va_end(argptr); */
+
+/*     // Actually print to stderr */
+/*     DrawText(messageString, (int)pos.x, (int)pos.y, 18, WHITE); */
+/* } */
---
+++ src/visdebugger.h
@@ -0,0 +1,10 @@
+#ifndef VISDEBUGGER_H_
+#define VISDEBUGGER_H_
+#include <raylib.h>
+
+void DebugDisplayText(Vector2 pos, const char *restrict format, ...);
+
+/* void DebugDisplayText2DWorld(Vector2 pos, const char *restrict format, ...); */
+
+
+#endif // VISDEBUGGER_H_
