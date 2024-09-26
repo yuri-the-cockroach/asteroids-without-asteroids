1e19a4c2ed9ec8a4f90c997ebf4836ef9b94cb50
LutzGebelman <yuraosipov2012@gmail.com>
2024-09-27 01:16:10 +0300

    Added logic to support menus

---
+++ src/menulogic.c
@@ -0,0 +1,76 @@
+#include "logger.h"
+#include "menulogic.h"
+#include "structs.h"
+
+void TriggerGameOver(void) {
+    GAME_STATE = GAME_OVER;
+}
+
+void TriggerNewGame(void) {
+    GAME_STATE = START_NEW;
+}
+
+void TriggerExitGame(void) {
+    GAME_STATE = EXIT;
+}
+
+int SelectCurrent(struct menuParent *menu) {
+    menu->optionList[menu->selected].MenuAction();
+    return 0;
+}
+
+int MenuStackPush(struct menuStack *stack, struct menuParent menu) {
+    if ( stack->stackSize >= MAX_MENU_STACK_SIZE ) {
+        LOG(ERROR, "%s", "Menu stack size at maximum capacity");
+        return -1;
+    }
+    stack->optionList[stack->stackSize] = menu;
+    stack->stackSize++;
+    return 0;
+}
+
+struct menuParent* MenuStackGet(struct menuStack *stack) {
+    if ( stack->stackSize == 0 ) {
+        LOG(WARNING, "%s", "Menu stack is empty. Returning empty struct");
+        return NULL;
+    }
+
+    return &stack->optionList[stack->stackSize - 1];
+}
+
+struct menuParent MenuStackPop(struct menuStack *stack) {
+    if ( stack->stackSize == 0 ) {
+        LOG(WARNING, "%s", "Menu stack is empty. Returning empty struct");
+        return (struct menuParent){ 0 };
+    }
+    stack->stackSize--;
+    struct menuParent toReturn = stack->optionList[stack->stackSize];
+
+    stack->optionList[stack->stackSize] = (struct menuParent){ 0 };
+    return toReturn;
+}
+
+int MenuStackClear(struct menuStack *stack) {
+    for ( int i = stack->stackSize - 1; i != 0; i-- ) {
+        MenuStackPop(stack);
+    }
+    return 0;
+}
+
+struct menuParent SetupPauseMenu(void){
+    const int numberOfItems = 2;
+    struct menuOption *optionList = calloc(numberOfItems, sizeof(struct menuOption));
+    optionList[0] = (struct menuOption){ "MAIN MENU", *TriggerGameOver };
+    optionList[1] = (struct menuOption){ "EXIT", *TriggerExitGame };
+
+    return (struct menuParent){ 0, numberOfItems, optionList};
+}
+
+struct menuParent SetupMainMenu(void) {
+    const int numberOfItems = 2;
+    struct menuOption *optionList = calloc(numberOfItems, sizeof(struct menuOption));
+    optionList[0] = (struct menuOption){ "START GAME", *TriggerNewGame };
+    optionList[1] = (struct menuOption){ "EXIT", *TriggerExitGame };
+
+    return (struct menuParent){ 0, numberOfItems, optionList};
+}
---
+++ src/menulogic.h
@@ -0,0 +1,34 @@
+#ifndef MENULOGIC_H_
+#define MENULOGIC_H_
+
+#include "structs.h"
+
+// Execute currently highlighted action
+int SelectCurrent(struct menuParent *menu);
+
+void TriggerGameOver(void);
+
+// Sets game state to NEW_GAME, because there's no lambda functions in C
+void TriggerNewGame(void);
+
+// Sets game state to EXIT, because there's no lambda functions in C
+void TriggerExitGame(void);
+
+// Push an entry to menu stack
+int MenuStackPush(struct menuStack *stack, struct menuParent menu);
+
+// Pop an entry from menu stack
+struct menuParent MenuStackPop(struct menuStack *stack);
+
+// Empty the menu stack
+int MenuStackClear(struct menuStack *stack);
+
+
+struct menuParent SetupMainMenu(void);
+
+struct menuParent SetupPauseMenu(void);
+
+// Get the entry from the top of the stack without removing it from the stack
+struct menuParent* MenuStackGet(struct menuStack *stack);
+
+#endif // MENULOGIC_H_
