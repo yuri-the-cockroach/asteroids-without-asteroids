58b9ae9b63ee4ec2e7f89da3baf924a3609fded7
LutzGebelman <yuraosipov2012@gmail.com>
2024-09-27 01:26:38 +0300

    Removed unused code and added more config options

--- src/visdebugger.h
+++ src/visdebugger.h
@@ -1,8 +1,8 @@
 #ifndef VISDEBUGGER_H_
 #define VISDEBUGGER_H_
-#include <raylib.h>
+#include "structs.h"
 
-void DebugDisplayText(Vector2 pos, const char *restrict format, ...);
+void DebugDisplayText(Vector2 pos, int fontSize, Color color, const char *restrict format, ...);
 
 /* void DebugDisplayText2DWorld(Vector2 pos, const char *restrict format, ...); */
 
