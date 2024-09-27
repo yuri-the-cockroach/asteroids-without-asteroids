893d1d180b4c64999bf99bae526ed55e424ac42d
LutzGebelman <yuraosipov2012@gmail.com>
2024-09-27 04:24:39 +0300

    Overflow/underflow functions
    If value goes over max, it will be set to min and vice-versa

--- src/asteroidsutils.c
+++ src/asteroidsutils.c
@@ -4,12 +4,25 @@ float GetRandomFloat(float min, float max) {
     return (float)(GetRandomValue((int)(min * 1000), (int)(max * 1000))) / 1000;
 }
 
+// Rollover the number, if it gets over max or under min
+float RollOverFloat(float d, float min, float max) {
+    const float t = d < min ? max : d;
+    return t > max ? min : t;
+}
+
+// Rollover the number, if it gets over max or under min
+int RollOverInt(int d, int min, int max) {
+    const int t = d < min ? max : d;
+    return t > max ? min : t;
+}
+
 // Clap the value D from min to max (I hate this code, but it's good in
 // assembly)
 float ClampFloat(float d, float min, float max) {
     const float t = d < min ? min : d;
     return t > max ? max : t;
 }
+
 
 int ClampInt(int d, int min, int max) {
     const int t = d < min ? min : d;
--- src/asteroidsutils.h
+++ src/asteroidsutils.h
@@ -6,10 +6,30 @@
 
 #define UNUSED(x) (void)(x)
 
+// Clap float from min to max
+// I hate this code
+// but it's good in assembly
 float ClampFloat(float d, float min, float max);
+
+// Clap int from min to max
+// I hate this code,
+// but it's good in assembly
 int ClampInt(int d, int min, int max);
+
+// Gets you a random float between min and max
+// Wrapper for raylib GetRandomValue
 float GetRandomFloat(float min, float max);
+
+// Get current time in milliseconds
 long GetTimeMS(void);
+
+// Cleanup memory find all NULLs in memory
+// remove them and rearenge memory
 void CleanupMemory(ObjectTracker *tracker);
 
+// Rollover int, if it gets over max or under min
+int RollOverInt(int d, int min, int max);
+
+// Rollover float, if it gets over max or under min
+float RollOverFloat(float d, float min, float max);
 #endif // UTILS_H_
