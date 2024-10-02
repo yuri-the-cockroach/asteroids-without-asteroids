fe37485aeb577efbba85df8e1da871a3ca449ed4
LutzGebelman <yuraosipov2012@gmail.com>
2024-10-03 02:35:25 +0300

    Benchmarking, CLEANUP state and file logging

--- src/statemachine.c
+++ src/statemachine.c
@@ -1,11 +1,18 @@
 #include "statemachine.h"
+#include "asteroidsutils.h"
 #include "gamelogic.h"
+#include "logger.h"
 #include "menulogic.h"
 #include "objecthandler.h"
+#include "render.h"
 #include "structs.h"
+#include "benchmarking.h"
+
 #include <raylib.h>
+#include <stdio.h>
 
 enum game_state GAME_STATE = INIT;
+enum game_state NEXT_STATE = NOOP;
 
 // Set default values for basic variables
 void RunConfig(void) {
@@ -17,20 +24,25 @@ void RunConfig(void) {
     SetWindowMaxSize(8192, 8192);
 
     SetTargetFPS(FPS_TARGET);
+    CreateLogFile();
 }
 
 int StateMachine(void) {
+    long timerStartTotalCycle = 0;
+    long timerStartRender = 0;
+    long timerStartKeys = 0;
 
-    RunConfig();
     ObjectTracker *tracker = NULL;
 
     struct menuParent mainMenu = SetupMainMenu();
     struct menuParent pauseMenu = SetupPauseMenu();
+    struct menuParent gameOverMenu = SetupGameOverMenu();
     /* struct menuStack mStack = (struct menuStack){ */
     /*     0, calloc(MAX_MENU_STACK_SIZE, sizeof(struct menuParent)) */
     /* }; */
 
     /* MenuStackPush(&mStack, mainMenu); */
+
 
     while (true) {
 
@@ -45,7 +57,7 @@ int StateMachine(void) {
 
             case MAIN_MENU: {
                 MenuControlls(&mainMenu);
-                RunMenuRender(&mainMenu, "ASTEROIDS WITHOUT ASTEROIDS");
+                RunMenuRender(&mainMenu, "ASTEROIDS WITHOUT ASTEROIDS", 0);
                 break;
             }
 
@@ -58,13 +70,17 @@ int StateMachine(void) {
             }
 
             case GAME_OVER: {
-                DeleteTracker(tracker);
-                GAME_STATE = MAIN_MENU;
+                char msg[128] = "";
+                sprintf(msg, "Your score: %d", tracker->playerScore);
+                MenuControlls(&gameOverMenu);
+                RunMenuRender(&gameOverMenu, "GAME OVER", 1, msg);
                 break;
             }
 
             case RUNNING: {
 
+                LOG(BENCH, "%s", "<--- Started frame cycle --->");
+                BenchStart(&timerStartTotalCycle);
                 if (DEBUGGING)
                     DebugingKeyHandler(tracker);
 
@@ -76,9 +92,14 @@ int StateMachine(void) {
                 if (!DEBUG_PAUSE)
                     RunActionList(tracker);
 
+                BenchStart(&timerStartRender);
                 // Rendering
                 RunWorldRender(tracker);
                 RunScreenRender(tracker);
+
+                BenchEnd(&timerStartRender, "Renderer");
+                BenchEnd(&timerStartTotalCycle, "Total cycle");
+                LOG(BENCH, "%s", "<--- Ended frame cycle --->\n");
                 break;
             }
 
@@ -94,12 +115,24 @@ int StateMachine(void) {
                 RunWorldRender(tracker);
                 RunScreenRender(tracker);
 
-                RunMenuRender(&pauseMenu, "GAME PAUSED");
+                RunMenuRender(&pauseMenu, "GAME PAUSED", 0);
                 break;
             }
 
             case EXIT: {
                 return 0;
+            }
+
+            case CLEANUP: {
+                DeleteTracker(tracker);
+                if ( NEXT_STATE == NOOP ) GAME_STATE = EXIT;
+                GAME_STATE = NEXT_STATE;
+                NEXT_STATE = NOOP;
+                break;
+            }
+
+            case NOOP: {
+                err(-1, "Game is stuck in noop! This state is not supposed to be used");
             }
         }
 
