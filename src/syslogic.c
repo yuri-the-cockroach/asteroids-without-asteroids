677e8068249cb796a15a3695ad55649e1a7dd6c5
LutzGebelman <yuraosipov2012@gmail.com>
2024-10-03 02:36:21 +0300

    Segfault handler and stack dumping in case of it

--- src/syslogic.c
+++ src/syslogic.c
@@ -1,5 +1,29 @@
 #include "syslogic.h"
+#include "structs.h"
 
 void SigIntHandler(int sig) {
     err(sig, "Got a signal interupt from console. Exiting now\n");
 }
+
+void SigSegvHandel(int sig) {
+    char sym[256];
+    unw_context_t context;
+    unw_cursor_t cursor;
+    unw_getcontext(&context);
+    unw_init_local(&cursor, &context);
+    struct unw_proc_info procInfo;
+
+    while (unw_step(&cursor) > 0) {
+        unw_word_t offset, pc;
+        unw_get_reg(&cursor, UNW_REG_IP, &pc);
+        if (pc == 0) {
+            break;
+        }
+        if (unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0) {
+            unw_get_proc_info(&cursor,&procInfo);
+            printf("%s+0x%lx\n", sym, offset);
+        }
+    }
+    err(sig, "Segmentation fault. Exiting now\n");
+}
+
--- src/syslogic.h
+++ src/syslogic.h
@@ -1,8 +1,13 @@
 #ifndef SYSLOGIC_H
 #define SYSLOGIC_H
 
+#define _XOPEN_SOURCE 700
+#define UNW_LOCAL_ONLY
+
+#include <libunwind.h>
 #include "structs.h"
 
 void SigIntHandler(int sig);
+void SigSegvHandel(int sig);
 
 #endif // SYSLOGIC_H
