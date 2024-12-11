ee269cdb4cee642f67e2c2fa0d8ed66e067e862c
LutzGebelman <yuraosipov2012@gmail.com>
2024-12-12 00:07:26 +0200

    Time is now counted only while the game is running

--- main.c
+++ main.c
@@ -19,8 +19,10 @@ int FPS_TARGET = 75;
 long LAST_SHOT = 0;
 bool CAMERA_FOLLOW = true;
 bool GDB_BREAK = false;
-long LAST_ASTEROID_SPAWN = 0;
-long NEXT_ASTEROID_SPAWN = 0;
+float LAST_ASTEROID_SPAWN = 0;
+float NEXT_ASTEROID_SPAWN = 0;
+float GAME_TIME_PASSED = 0;
+Vector2 SPEED_PREV = {0, 0};
 
 #ifdef DEBUGGING
     bool DEBUG_PAUSE = false;
