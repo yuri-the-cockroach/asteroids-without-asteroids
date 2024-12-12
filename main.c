97646f7c7afff7ec9932e1491745556964f02da6
LutzGebelman <yuraosipov2012@gmail.com>
2024-12-12 03:18:19 +0200

    I can't provide better debug info than sanitizer..

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ int main(int argc, char **argv) {
     // Handle the interseption of signals
     signal(SIGINT, SigIntHandler);
     signal(SIGTERM, SigIntHandler);
-    signal(SIGSEGV, SigSegvHandel);
+    // signal(SIGSEGV, SigSegvHandel);
 
     StateMachine();
     fclose(LOG_FILE_PTR);
