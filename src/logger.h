65d350a71ca796b716e5cf66325f85c1bcfa2088
LutzGebelman <yuraosipov2012@gmail.com>
2024-12-22 18:18:23 +0200

    Reformat

--- src/logger.h
+++ src/logger.h
@@ -2,10 +2,12 @@
 #define LOGGER_H_
 #include "structs.h"
 
-#pragma GCC diagnostic ignored "-Wformat-nonliteral" // Ignore, because format is const char *restrict, so it's not an issue
+#pragma GCC diagnostic ignored                                                 \
+    "-Wformat-nonliteral" // Ignore, because format is const char *restrict, so
+                          // it's not an issue
 
-#define LOG(logLevel, format, ...) Logger(__FILE__, __FUNCTION__, __LINE__, logLevel, format, __VA_ARGS__)
-
+#define LOG(logLevel, format, ...)                                             \
+    Logger(__FILE__, __FUNCTION__, __LINE__, logLevel, format, __VA_ARGS__)
 
 void Logger(const char *restrict inFile, const char *restrict inFunc,
             const int onLine, const enum loglevel loglevel,
