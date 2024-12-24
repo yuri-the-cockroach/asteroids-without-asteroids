1a5bf5cc45fa2ade596abd9bf72d74b4b3a978ee
LutzGebelman <yuraosipov2012@gmail.com>
2024-12-25 01:32:22 +0200

    Reformat & remove `#ifdef DEBUGGING`

--- src/visdebugger.h
+++ src/visdebugger.h
@@ -1,9 +1,8 @@
-#ifdef DEBUGGING
 #ifndef VISDEBUGGER_H_
 #define VISDEBUGGER_H_
-#include "structs.h"
+#include <raylib.h>
 
-void DebugDisplayText(Vector2 pos, int fontSize, Color color, const char *restrict format, ...);
+void DebugDisplayText(Vector2 pos, int fontSize, Color color,
+                      const char *restrict format, ...);
 
 #endif // VISDEBUGGER_H_
-#endif // DEBUGGING
