f10598d8d7f6a7fcae72bf002ed4a5daedeffa8e
LutzGebelman <yuraosipov2012@gmail.com>
2024-12-26 02:33:29 +0200

    Auto-reformat & set N_CPU_THREADS=1 by default

--- src/structs.c
+++ src/structs.c
@@ -1,39 +1,40 @@
 #include "structs.h"
 
 #ifndef DEFAULTS
-#define DEFAULTS
+    #define DEFAULTS
 
 enum loglevel CURRENT_LOG_LEVEL_CONSOLE = DEFAULT_LOG_LEVEL;
-enum loglevel CURRENT_LOG_LEVEL_FILE = DEFAULT_LOG_LEVEL;
+enum loglevel CURRENT_LOG_LEVEL_FILE    = DEFAULT_LOG_LEVEL;
 
-int SCREEN_WIDTH = 1600;
+int SCREEN_WIDTH  = 1600;
 int SCREEN_HEIGHT = 900;
 
 int FPS_TARGET = 75;
 
-long LAST_SHOT = 0;
-bool CAMERA_FOLLOW = true;
-float LAST_ASTEROID_SPAWN = 0;
-float NEXT_ASTEROID_SPAWN = 0;
-float GAME_TIME_PASSED = 0;
+long LAST_SHOT                 = 0;
+bool CAMERA_FOLLOW             = true;
+float LAST_ASTEROID_SPAWN      = 0;
+float NEXT_ASTEROID_SPAWN      = 0;
+float GAME_TIME_PASSED         = 0;
 enum difficulty CUR_DIFFICULTY = 0;
 
-#ifdef DEBUGGING
-objWrap *lastDragged = NULL;
-Vector2 SPEED_PREV = { 0, 0 };
-bool GDB_BREAK = false;
-bool DEBUG_PAUSE = false;
+    #ifdef DEBUGGING
+objWrap *lastDragged          = NULL;
+Vector2 SPEED_PREV            = { 0, 0 };
+bool GDB_BREAK                = false;
+bool DEBUG_PAUSE              = false;
 bool VISUAL_DEBUG_SHOW_POINTS = false;
-bool VISUAL_DEBUG = false;
-#endif // DEBUGGING
+bool VISUAL_DEBUG             = false;
+    #endif // DEBUGGING
 
-#ifdef BENCHMARKING
-bool BENCHRUNNING = false;
+    #ifdef BENCHMARKING
+bool BENCHRUNNING        = false;
 long BENCH_COLLIDER_TIME = 0;
 FILE *BENCH_LOG_FILE_PTR;
-#endif // BENCHMARKING
+    #endif // BENCHMARKING
 
 char LOG_FILE_NAME[64];
 FILE *LOG_FILE_PTR;
 
+long N_CPU_THREADS = 1;
 #endif
