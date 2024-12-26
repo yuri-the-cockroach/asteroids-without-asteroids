bffbc19bb1ceb7f8b3193902a67b30dff4a0132a
LutzGebelman <yuraosipov2012@gmail.com>
2024-12-26 03:14:04 +0200

    Remove requirement for extra an arg

--- src/logger.h
+++ src/logger.h
@@ -2,15 +2,14 @@
 #define LOGGER_H_
 #include "structs.h"
 
-#pragma GCC diagnostic ignored                                                 \
+#pragma GCC diagnostic ignored \
     "-Wformat-nonliteral" // Ignore, because format is const char *restrict, so
                           // it's not an issue
 
-#define LOG(logLevel, format, ...)                                             \
-    Logger(__FILE__, __FUNCTION__, __LINE__, logLevel, format, __VA_ARGS__)
+#define LOG(logLevel, ...) \
+    Logger(__FILE__, __FUNCTION__, __LINE__, logLevel, __VA_ARGS__)
 
 void Logger(const char *restrict inFile, const char *restrict inFunc,
-            const int onLine, const enum loglevel loglevel,
-            const char *restrict format, ...);
+            const int onLine, const enum loglevel loglevel, ...);
 
 #endif // LOGGER_H_
