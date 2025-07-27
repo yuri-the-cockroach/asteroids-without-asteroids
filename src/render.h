d16ec716fc4e674562ba900dc323f802c0a44041
YuriTheCockroach <yuraosipov2012@gmail.com>
2025-07-27 21:38:47 +0300

    This function was never used

--- src/render.h
+++ src/render.h
@@ -7,11 +7,6 @@
 void DisplayText(Vector2 pos, int fontSize, Color color,
                  const char *restrict format, ...);
 
-// Calls DrawObject on each object in tracker->drawList
-// nulls it's position in the list after the object is draw
-// and finnaly sets tracker->drawListLen to 0 when everything is done
-void DrawAllFromDrawList(objTracker *tracker);
-
 // Draws the object based on it's *object.shape.points* array
 void DrawObject(objWrap *wrap);
 
