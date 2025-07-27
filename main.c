74de7bb336332a63d13e5876eebcc8e3d0d614ef
YuriTheCockroach <yuraosipov2012@gmail.com>
2025-07-27 19:46:36 +0300

    Add call to InitRaylib

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(int argc, char **argv) {
 
     // Handle the startup arguments
     RunConfig();
+    InitRaylib();
     GetStartUpArguments(argc, argv);
 
     CURRENT_LOG_LEVEL_FILE = CURRENT_LOG_LEVEL_FILE < CURRENT_LOG_LEVEL_CONSOLE
