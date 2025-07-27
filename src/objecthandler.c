25f12d343ffb2e4172380d9fa6f42f60922de328
YuriTheCockroach <yuraosipov2012@gmail.com>
2025-07-27 19:14:16 +0300

    Remove UpdateObj function
    This function was folded into the handler itself

--- src/objecthandler.c
+++ src/objecthandler.c
@@ -55,19 +55,6 @@ void RunActionList(objTracker *tracker) {
     CleanupMemory(tracker);
 }
 
-void UpdateObj(objTracker *tracker, unsigned long index) {
-
-    objWrap *wrap = tracker->objList[index];
-
-#ifndef MT_ENABLED
-    if (wrap->collider.isCollidable) FastFindCollisions(tracker, index);
-#endif // MT_ENABLED
-
-    if (wrap->isRotatableByGame) {
-        RotateObject(wrap, (wrap->objPtr->rotateSpeed));
-    }
-
-    if (wrap->updatePosition) UpdateObjectPos(wrap);
 }
 
 objTracker *InitTracker(void) {
